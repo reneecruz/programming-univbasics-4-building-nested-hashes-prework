a1326a6eedf20474d02d44017568d0c224198b56
Renée Cruz <reneebasscruz@gmail.com>
2019-07-22 22:31:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,7 @@ def monopoly_with_fourth_tier
       :pieces => 4,
       :names => {
         :reading_railroad => {
-          "mortgage value" => "$100"
+          'mortgage value' => '$100'
         },
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
