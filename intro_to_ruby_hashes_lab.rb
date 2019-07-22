5def5c12aded59fab99ac29cf1b942824bcad0bf
Renée Cruz <reneebasscruz@gmail.com>
2019-07-22 22:20:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_third_tier
       :rent_in_dollars => {
         :one_piece_owned => 25,
         :two_pieces_owned => 50,
-        :three_
+        :three_pieces_owned => 100
         
       }
     },
