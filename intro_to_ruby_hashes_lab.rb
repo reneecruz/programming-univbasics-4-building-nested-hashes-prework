39bd456292b225d9231ffbb08c09909e52d3d9d3
Renée Cruz <reneebasscruz@gmail.com>
2019-07-22 22:17:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,10 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :names => {},
-      :rent_in_dollars => {:one_piece_owned => 25}
+      :rent_in_dollars => {
+        :one_piece_owned => 25
+        
+      }
     },
 	}
 
